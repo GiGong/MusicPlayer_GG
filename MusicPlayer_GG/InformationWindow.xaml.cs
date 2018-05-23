16f950d72c2a02c6cd6a8296037c39074bb2ccd0
GiGong <gigong222@gmail.com>
2018-05-23 19:47:55 +0900

    Button Style을 App.xaml파일로 이동, 정보 Window 디자인 수정 중

--- MusicPlayer_GG/InformationWindow.xaml.cs
+++ MusicPlayer_GG/InformationWindow.xaml.cs
@@ -6,6 +6,7 @@ See the file license.txt for copying permission.
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -37,6 +38,8 @@ namespace MusicPlayer_GG
             textCopyright.Text = "Copyright © 2018 GiGong.\nAll Rights Reserved.";
         }
 
+        #region Event For Information
+
         private void Event_OK(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
@@ -60,5 +63,117 @@ namespace MusicPlayer_GG
             }
             catch { }
         }
+
+        private void Event_GitHub(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://github.com/GiGong/MusicPlayer_GG");
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region Region For Custom Title Bar
+
+        private Point startPos;
+        System.Windows.Forms.Screen[] screens = System.Windows.Forms.Screen.AllScreens;
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            int sum = 0;
+            foreach (var item in screens)
+            {
+                sum += item.WorkingArea.Width;
+                if (sum >= this.Left + this.Width / 2)
+                {
+                    this.MaxHeight = item.WorkingArea.Height;
+                    break;
+                }
+            }
+        }
+
+        private void System_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (e.ClickCount >= 2)
+                {
+                    this.WindowState = (this.WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+                }
+                else
+                {
+                    startPos = e.GetPosition(null);
+                }
+            }
+            else if (e.ChangedButton == MouseButton.Right)
+            {
+                var pos = PointToScreen(e.GetPosition(this));
+                IntPtr hWnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+                IntPtr hMenu = GetSystemMenu(hWnd, false);
+                int cmd = TrackPopupMenu(hMenu, 0x100, (int)pos.X, (int)pos.Y, 0, hWnd, IntPtr.Zero);
+                if (cmd > 0) SendMessage(hWnd, 0x112, (IntPtr)cmd, IntPtr.Zero);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+        [DllImport("user32.dll")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32.dll")]
+        static extern int TrackPopupMenu(IntPtr hMenu, uint uFlags, int x, int y, int nReserved, IntPtr hWnd, IntPtr prcRect);
+
+        private void System_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (this.WindowState == WindowState.Maximized && Math.Abs(startPos.Y - e.GetPosition(null).Y) > 2)
+                {
+                    var point = PointToScreen(e.GetPosition(null));
+
+                    this.WindowState = WindowState.Normal;
+
+                    this.Left = point.X - this.ActualWidth / 2;
+                    this.Top = point.Y - border.ActualHeight / 2;
+                }
+                DragMove();
+            }
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = (this.WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Mimimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                main.BorderThickness = new Thickness(0);
+                main.Margin = new Thickness(7, 7, 7, 0);
+                //rectMax.Visibility = Visibility.Hidden;
+                //rectMin.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                main.BorderThickness = new Thickness(1);
+                main.Margin = new Thickness(0);
+                //rectMax.Visibility = Visibility.Visible;
+                //rectMin.Visibility = Visibility.Hidden;
+            }
+        }
+
+        #endregion
     }
 }
